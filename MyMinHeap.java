67b51b87574c924969ca9533ba681a812c2e5e52
Jamshed Ashurov <jamshed00ashurov@gmail.com>
2020-05-11 15:20:21 -0700

    2

--- MyMinHeap.java
+++ MyMinHeap.java
@@ -15,11 +15,13 @@ import java.util.Collection;
 /**
  * This class creates a min heap, a binary tree with two properties:
  * 1) The tree is complete. 
- * 2) The key of a node is not greater the keys of its children. 
+ * 2)) The key of a node is not greater the keys of its children. 
  */
 public class MyMinHeap<E extends Comparable<E>>
 {
     protected List<E> list; //The underlying data strcuture of the heap
+
+    private final static int TWO = 2; //used to find the parent/child indices
     
     /**
      * Constructor of the class that simply intializes an empty arraylist.
@@ -29,46 +31,83 @@ public class MyMinHeap<E extends Comparable<E>>
     }
     /**
      * Constructor of the class that moves the collection into the array.
+     *
+     * @param collection - the provided array 
      * Throws exceptions for invalid(null) input. 
      */
     public MyMinHeap(Collection<? extends E> collection) {
-
+        this();
         if (collection == null || collection.contains(null) == true) {
             throw new NullPointerException(); 
         }
         list = new ArrayList<>(collection); 
         for (int i = this.list.size() - 1; i <= 0; i--) {
+            System.out.println("check"); 
             percolateDown(i); 
         }
     }
 
+    /**
+     * A helper method that swaps the values of the two indicated indices.
+     *
+     * @param from - the index to swap
+     * @param to - the index to swap
+     * @return null
+     */
     protected void swap(int from, int to) {
         E temp = this.list.get(from); 
         this.list.set(from, this.list.get(to));
         this.list.set(to, temp); 
     }
-
+    
+    /**
+     * A helper method that returns the parent index of the specified key.
+     *
+     * @param index - the index of the key
+     * @return The Parent index
+     */
     protected int getParentIdx(int index) {
-        return ((index - 1) / 2); 
+        return ((index - 1) / TWO); 
     }
-
+    
+    /**
+     * A helper method that returns the left child index 
+     * of the specified key.
+     *
+     * @param index - the index of the key
+     * @return The left child index
+     */
     protected int getLeftChildIdx(int index) {
-        return (2 * (index) + 1); 
+        return (TWO * (index) + 1); 
     }
-
+    
+    /**
+     * A helper method that returns the right child index 
+     * of the specified key.
+     *
+     * @param index - the index of the key
+     * @return The right child index
+     */
     protected int getRightChildIdx(int index) {
-        return (2 * (index) + 2);
+        return (TWO * (index) + TWO);
     }
-
+    
+    /**
+     * A helper method that returns the index of the smaller child 
+     * of the specified key.
+     *
+     * @param index - the index of the key
+     * @return The index of the smaller child
+     */
     protected int getMinChildIdx(int index) {
         int l_ind = getLeftChildIdx(index); //left child index
         int r_ind = getRightChildIdx(index); //right child index
         
         if (l_ind > this.list.size() - 1) {
-            return -1;
+            return -1; //No children 
         }
         else if (l_ind == this.list.size() - 1) {
-            return l_ind; 
+            return l_ind; //Only one child
         }
         else {
             E l_value = this.list.get(l_ind); //left child value
@@ -86,6 +125,14 @@ public class MyMinHeap<E extends Comparable<E>>
         }
     }
     
+    /**
+     * A helper method that reorders the tree to match the property
+     * of the min heap when going up the tree at the specified key. 
+     * This method uses recursion. 
+     *
+     * @param index - the index of the key
+     * @return null
+     */
     protected void percolateUp(int index) {
         if (index == 0) {
             return; 
@@ -103,7 +150,15 @@ public class MyMinHeap<E extends Comparable<E>>
             }
         }
     }
-
+    
+    /**
+     * A helper method that reorders the tree to match the property
+     * of the min heap when going down the tree at the specified key. 
+     * This method uses recursion. 
+     *
+     * @param index - the index of the key
+     * @return null
+     */
     protected void percolateDown(int index) {
         int min_index = getMinChildIdx(index); //smaller child index
         if (min_index == -1) {
@@ -116,16 +171,24 @@ public class MyMinHeap<E extends Comparable<E>>
         }
         else {
             swap(index, min_index); 
-            percolateDown(min_index); 
+            percolateDown(min_index); //recursion
         }
     }
     
+    /**
+     * A helper method that deletes an element at the specified key.
+     * Reorders the tree using percolate up and down helper methods
+     * after the deletion. 
+     *
+     * @param index - the index of the key
+     * @return The deleted element
+     */
     protected E deleteIndex(int index) {
         if (this.list.size() == 1) {
-            return (this.list.remove(0));
+            return (this.list.remove(0)); //if only one element in the heap
         }
         else {
-            swap(this.list.size() - 1, index); //check with println if true
+            swap(this.list.size() - 1, index); //swaps with the last element
             E del_val = this.list.remove(this.list.size() - 1); 
             E replace_val = this.list.get(index); 
             if (replace_val.compareTo(del_val) < 0) {
@@ -137,7 +200,16 @@ public class MyMinHeap<E extends Comparable<E>>
             return del_val; 
         }
     }
-
+    
+    /**
+     * A core method of the heap that inserts an element to the tree.
+     * Inserts from the buttom of the tree and reorders the tree 
+     * using percolate up helper method. 
+     * Throws an exception for invalid input. 
+     *
+     * @param element - the element to be inserted 
+     * @return null
+     */
     public void insert(E element) {
         if (element == null) {
             throw new NullPointerException(); 
@@ -145,29 +217,51 @@ public class MyMinHeap<E extends Comparable<E>>
         this.list.add(element); 
         percolateUp(this.list.size() - 1); //Check with println if true
     }
-
+    
+    /**
+     * A core method that returns the minimum/higherst prirotity element 
+     * of the tree/array.
+     *
+     * @return Null if the heap is empty or the element if not
+     */
     public E getMin() {
-        if (this.list.size() == 0) {
+        if (this.list.size() == 0) { 
             return null; 
         }
         else {
-            return (this.list.get(0));
+            return (this.list.get(0));//the priority item will always be first
         }
     }
-
+    
+    /**
+     * The core method that removes the highest priority item of the heap.
+     * Uses deleteIndex helper method to remove the item. 
+     *
+     * @return the removed priority item
+     */
     public E remove() {
         if (this.list.size() == 0) {
             return null; 
         } 
         else {
-            return (deleteIndex(0)); 
+            return (deleteIndex(0)); //priority item will always be first
         }
     }
-
+    
+    /**
+     * The core method that returns the size of the heap.
+     *
+     * @return The size of the heap.
+     */
     public int size() {
         return (this.list.size()); 
     }
-
+    
+    /**
+     * The core method that removes all the elements from the heap.
+     *
+     * @return null
+     */
     public void clear() {
         this.list.clear(); 
         //check if the list is empty but not null
--- MyPriorityQueue.java
+++ MyPriorityQueue.java
@@ -9,40 +9,81 @@ import java.util.Collection;
  * Login: cs12sp20aqk
  */
 
+/**
+ * This class creates a priority queue using the MyMinHeap class and
+ * its methods.
+ */
 public class MyPriorityQueue<E extends Comparable<E>> {
     
-    protected MyMinHeap<E> heap; 
+    protected MyMinHeap<E> heap; //the heap that holds the elements of queue
 
+    /**
+     * Constructor that simply initializes the priorityqueue(heap)
+     */
     public MyPriorityQueue() {
         heap = new MyMinHeap<>(); 
     }
 
+    /**
+     * Constructor that stores the elements of the given collection to the 
+     * priority queue. 
+     * Throws an exception for invalid input. 
+     *
+     * @param collection - the given array of elements 
+     */
     public MyPriorityQueue(Collection<? extends E> collection) {
         if (collection == null || collection.contains(null) == true) {
             throw new NullPointerException();
         }
         heap = new MyMinHeap<>(collection);
     }
-
+    
+    /**
+     * Inserts the elements to the back of the queue.
+     * Throws an exception for invalid input.
+     *
+     * @param element - The element to be inserted
+     * @return null
+     */
     public void push(E element) {
         if (element == null) {
             throw new NullPointerException(); 
         }
         heap.insert(element); 
     }
-
+    
+    /**
+     * Returns the highest priority element of the queue.
+     *
+     * @return the priority element
+     */
     public E peek() {
         return (heap.getMin());
     }
-
+    
+    /**
+     * Removes the highest priority element of the queue. 
+     *
+     * @return the highest priority element.
+     */
     public E pop() {
         return (heap.remove()); 
     }
-
+    
+    /**
+     * Retunrs the size of the priority queue.
+     *
+     * @return the size of the queue. 
+     */
     public int getLength() {
         return (heap.size());
     }
-
+    
+    /**
+     * Removes all the elements from the queue. 
+     *
+     * @return null
+     */
     public void clear() {
         heap.clear();
     }
